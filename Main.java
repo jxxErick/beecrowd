36124a6521aa910ef36696e73b6e3db21ce4f3e2
jxxErick <ericksdantas144@gmail.com>
2023-05-29 19:33:37 -0300

    1000 - 1001

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+    // ex 1000
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.printHelloWorld();
+    // ex 1001
+    SomaDoisValores somaDoisValores = new SomaDoisValores();
+    somaDoisValores.soma();
+
+
+    }
+
+}
---
+++ SomaDoisValores.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SomaDoisValores {
+
+    public void soma(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escreva os valores a serem somados");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.println("x = " + c);
+    }
+}
